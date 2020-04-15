7d74dc08b19b9ac91bb5e446b13fa5141c39fe75
randor12 <rynicholas@vt.edu>
2020-04-15 12:56:25 -0400

    initialized first attempt at smart assistant

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+"""
+:author: Ryan Nicholas
+:date: April 15, 2020
+:description: First Smart Assistant
+"""
+
---
+++ src/main.py
@@ -0,0 +1,6 @@
+"""
+:author: Ryan Nicholas
+:date: April 15, 2020
+:description: Attempt at creating a smart assistant
+"""
+
