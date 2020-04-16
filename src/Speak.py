41f71bf60d23d0d21fc6dd63271e88c70208b61b
randor12 <rynicholas@vt.edu>
2020-04-15 21:37:50 -0400

    Added speaking class

---
+++ src/Speak.py
@@ -0,0 +1,25 @@
+"""
+:author: Ryan Nicholas
+:date: April 15, 2020
+:description: File that allows Randor Assistance to respond to speech requests
+"""
+
+class Speak:
+    def __init__(self):
+        """
+        Initialize Speak Class that allows for text-to-speech functionality
+        """
+
+    def respond(self, msg):
+        """
+        Respond with a certain message
+        :param msg: message to respond with
+        :return: None
+        """
+
+    def search(self, msg):
+        """
+        Search the web for a certain request
+        :param msg: request to search the internet
+        :return: None
+        """
