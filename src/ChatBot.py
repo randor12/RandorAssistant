1d894b46226cfb2d476742ad6dc6552b5c249816
randor12 <rynicholas@vt.edu>
2020-04-16 16:36:49 -0400

    Worked on Chat Bot

--- src/ChatBot.py
+++ src/ChatBot.py
@@ -11,14 +11,24 @@ class ChatBot:
         Initialize chat bot
         """
 
-    def sendResponse(self, msg):
+    def send_response(self, msg):
         """
         Send a response to a message
         :param msg: message sent from the user
         :return: return the response to the message
         """
         response = ""
-
-
-
+        answer = self.use_model(msg)
+        if answer is not None and len(answer) > 0:
+            response = answer
         return response
+
+    @staticmethod
+    def use_model(msg):
+        """
+        Use ChatBot model in order to come up with a response and message
+        :param msg: message sent
+        :return: response to the message
+        """
+        r = msg
+        return r
