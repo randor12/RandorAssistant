229bde3799c5e983d9b16dd22a37dfb7a109ddfc
randor12 <rynicholas@vt.edu>
2020-04-21 23:13:15 -0400

    added scraper class to search the internet

---
+++ src/Scraper.py
@@ -0,0 +1,22 @@
+"""
+Class in order to allow for internet scraping on any topic specified
+@author Ryan Nicholas
+@version 2020.04.21
+"""
+
+# SOURCE FOR LATER: https://scrapy.org/
+# Stack Overflow: https://stackoverflow.com/questions/15798878/how-to-search-internet-with-python
+
+
+class Scraper:
+    def __init__(self):
+        """
+        Initialize the scraper
+        """
+
+    def search(self, topic):
+        """
+        Search for a topic and scrape the internet for data on the topic
+        :param topic: topic to scrape
+        :return: None
+        """
