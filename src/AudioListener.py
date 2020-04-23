644a7bfa2663eacc9222aa9b5442f01542582f6a
randor12 <rynicholas@vt.edu>
2020-04-23 11:44:02 -0400

    Attempting Callback Function in Background to improve Smart Assistant

--- src/AudioListener.py
+++ src/AudioListener.py
@@ -12,6 +12,11 @@
 import speech_recognition as sr
 import os
 import json
+import asyncio
+import time
+from Speaker import Speaker as sp
+from command import Commands as cmd
+import random
 
 
 class AudioListener:
@@ -25,6 +30,12 @@ class AudioListener:
         #     self.GOOGLE_CLOUD_SPEECH_CREDENTIALS = r"""{}""".format(val)
         #     self.GOOGLE_CLOUD_SPEECH_CREDENTIALS = self.GOOGLE_CLOUD_SPEECH_CREDENTIALS.replace("\'", "\"")
         # print("Google Auth JSON: ", self.GOOGLE_CLOUD_SPEECH_CREDENTIALS)  # display location of GOOGLE AUTH JSON
+        self.r = sr.Recognizer()
+        self.source = sr.Microphone()
+        self.text = ""
+        self.command = False
+        self.speakingStartResponses = ['go ahead ryan', 'i am listening',
+                                    'what is it ryan', 'how can i help ryan']
 
     def startMicrophone(self):
         """
@@ -44,13 +55,64 @@ class AudioListener:
         try:
             # attempt to recognize the speech
             text = r.recognize_google(audio, language='en-US')
-            print("Google Cloud Speech thinks you said: ", text)
+            print("Randor Assistant thinks you said: ", text)
         except sr.UnknownValueError:
-            print("Google Cloud Speech could not understand audio")
+            print("Randor Assistant could not understand audio")
         except sr.RequestError as e:
-            print("Could not request results from Google Cloud Speech service; {0}".format(e))
+            print("Could not request results from Google services; {0}".format(e))
 
         return text
+
+    def callback(self, recognizer, audio):
+        """
+        Callback funtion to recognize noise in the background
+        :return: None
+        """
+        previousText = self.text
+        try:
+            self.text = recognizer.recognize_google(audio, language='en-US')
+            print("Randor Assistant thinks you said: ", self.text)
+        except sr.UnknownValueError:
+            self.text = previousText
+            print("Randor Assistant Could Not Understand You")
+        except sr.RequestError as e:
+            self.text = previousText
+            print("Could not request results form Google services; {0}".format(e))
+
+        if self.text not None and 'randor' in self.text and not self.command:
+            self.command = True
+            speaking = sp()
+            speakResponse = random.choice(self.speakingStartResponses)
+            speaking.respond(speakResponse)
+
+        if self.command:
+            response = cmd(self.text)
+            self.command = False
+
+
+
+    async def backgroundListener(self):
+        """
+        Start listening from the background and call a
+        callback function in order to allow for the assistant to
+        communicate
+        :return: Return the text that is sent from the background noise
+        """
+        stop_audio = self.r.listen_in_background(self.source, self.callback)
+        previousText = ""
+        sameAsBefore = True
+        while 'exit' not in self.text:
+            if previousText == self.text:
+                sameAsBefore = True
+            else:
+                sameAsBefore = False
+                previousText = self.text
+            await asyncio.sleep(1)
+            if self.text not None and not sameAsBefore:
+                yield self.text
+            time.sleep(0.1)
+
+        stop_audio()
 
 # ex = AudioListener()
 #
